7fbcc7a4c346131fd9a4c6d25d6869b9a9b7f685
Greg Farrell <greg@gregfarrell.org>
2018-09-30 15:17:17 +0100

    add trailing comma

--- app/models/diceroll.js
+++ app/models/diceroll.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     count: DS.attr('number'),
     type: DS.attr('number'),
-    table: DS.belongsTo('table', { async: false })
+    table: DS.belongsTo('table', { async: false }),
 });
