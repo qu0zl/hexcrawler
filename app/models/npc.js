f707a1cc43a873c5b5699a386a559f177e921225
Greg Farrell <greg@gregfarrell.org>
2018-10-28 22:03:02 +0000

    More WIP towards NPC lists

--- app/models/npc.js
+++ app/models/npc.js
@@ -3,15 +3,17 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     name: DS.attr(),
     HD: DS.attr(),
+    HP: DS.attr(),
     AC: DS.attr(),
     attack: DS.attr(),
-    move: DS.attr('number'),
+    move: DS.attr(),
     save: DS.attr('number'),
     AL: DS.attr(),
     CL: DS.attr('number'),
     XP: DS.attr('number'),
     equipment: DS.attr(),
     special: DS.attr(),
+    spells: DS.attr(),
     renderAC() {
         if (typeof(this.AC) == "string")
             return this.AC
@@ -20,7 +22,16 @@ export default DS.Model.extend({
     renderEquipment() {
         return this.equipment ? `<br>&nbsp<b>Equipment: </b>${this.equipment}` : "";
     },
+    renderHP() {
+        return this.HP ? `<b>HP </b>${this.HP}; ` : "";
+    },
+    renderSpells() {
+        return this.spells ? `<br><b>Spells:</b> ${this.spells} ` : "";
+    },
+    renderSpecial() {
+        return this.special ? `<b>Special:</b> ${this.special} ` : "";
+    },
     render() {
-        return `<br><b>${this.name}</b>:<b>HD </b>${this.HD}; <b>AC </b> ${this.renderAC()}; <b>Atk </b> ${this.attack}; <b>Move </b>${this.move}; <b>Save </b>${this.save}; <b>AL </b>${this.AL}; <b>CL/XP </b>${this.CL}/${this.XP}; <b>Special</b>: ${this.special} ${this.renderEquipment()}`
+        return `<br><b>${this.name}</b>: <b>HD </b>${this.HD}; <b>AC </b> ${this.renderAC()}; <b>Atk </b> ${this.attack}; <b>Move </b>${this.move}; <b>Save </b>${this.save}; <b>AL </b>${this.AL}; <b>CL/XP </b>${this.CL}/${this.XP}; ${this.renderSpecial()}${this.renderSpells()}${this.renderEquipment()}`
     },
 });
