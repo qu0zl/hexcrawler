0f81b9921af0347e7f50d75a37dc9fcc4e2432d9
Greg Farrell <greg@gregfarrell.org>
2018-10-29 18:34:54 +0000

    Add secondary passage

--- mirage/fixtures/tables.js
+++ mirage/fixtures/tables.js
@@ -1,5 +1,6 @@
 export default [
     {id: "PP", title: 'Primary Passage', dicerollId: "1d40", tableItemIds: ["PP0","PP1","PP2","PP3","PP4","PP5","PP6","PP7","PP8","PP9","PP10","PP11","PP12","PP13","PP14","PP15","PP16","PP17","PP18","PP19","PP20","PP21"] },
+    {id: "SecondaryPassage", title: 'Secondary Passage', dicerollId: "1d40", tableItemIds: ["SP0","SP1","SP2","SP3","SP4","SP5","SP6","SP7","SP8","SP9","SP10","SP11","SP12","SP13","SP14","SP15","SP16","SP17","SP18","SP19","SP20","SP21"] },
     {id: "Caravan", title: 'Caravan', dicerollId: "1d100", tableItemIds: ["Car1","Car2","Car3","Car4","Car5"] },
     /*
     {id: "HJE", title: 'Heavy Jungle Elemental', dicerollId: "3d6" },
