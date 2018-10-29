d92d0abd2cc122d93f7e30724a445087a6708a34
Greg Farrell <greg@gregfarrell.org>
2018-10-29 22:45:32 +0000

    try to return necessary from tables routes

--- app/routes/tables.js
+++ app/routes/tables.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model() {
-        return this.store.findAll('table', { include: 'subTables, diceroll, tableItems, tableItems.encounters, tableItems.encounters.npcs' });
+        return this.store.findAll('table', { include: 'subTables, subTables.tableItems, subTables.tableItems.encounters, diceroll, tableItems, tableItems.encounters, tableItems.encounters.npcs' });
     }
 });
